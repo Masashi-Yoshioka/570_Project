2b7a408b80409f354cf1b7c3db987d47438bf5ce
Masashi-Yoshioka <masashiy@usc.edu>
2022-05-06 03:51:19 -0700

    0506
    Created a new notebook that basically replicates the original one

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,13 +1,1 @@
-import numpy as np
-
-# Covariance matrix of X
-def fn_generate_cov(dim, corr):
-    
-    acc  = []
-    for i in range(dim):
-        row = np.ones((1, dim)) * corr
-        row[0][i] = 1
-        acc.append(row)
-    
-    return np.concatenate(acc, axis = 0)
 
